d71aab87871200abc0ea902526c8379fa8394410
xenwithu <lvd.levandat@gmail.com>
2023-11-08 10:35:32 +0700

    feat: call api register and handle error"

--- src/services/Account.js
+++ src/services/Account.js
@@ -3,7 +3,21 @@ import axios from 'axios';
 const Account = {
     signIn: (username, password, type) => {
         console.log("signin");
-        return axios.post('http://27.69.251.31:9000/api/v1/users/signin', { username: username, password: password, type: type })
+        return axios.post('http://ridewizard.pro:9000/api/v1/users/signin', { username: username, password: password, type: type })
+    },
+    signUp: async({ name, phNo, email, password }) => {
+        console.log("signup");
+        return await axios({
+                method: 'post',
+                url: 'http://ridewizard.pro:9000/api/v1/users/signup',
+                data: { name, phNo, email, password },
+            })
+            .then((response) => {
+                return response;
+            })
+            .catch((error) => {
+                return error.response;
+            });
     }
 }
 
